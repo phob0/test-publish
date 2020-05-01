edf4a33ab7d4fe64dfc8e2eeee62659d6ff06e0d
Serban <sharebaanda@gmail.com>
2020-05-01 03:45:10 +0300

    env and register

--- src/TestPublishServiceProvider.php
+++ src/TestPublishServiceProvider.php
@@ -12,11 +12,22 @@ class TestPublishServiceProvider extends ServiceProvider
             __DIR__.'/../config/translatable.php' => config_path('translatable.php'),
         ], 'config');
 
+        $this->publishes([
+            __DIR__.'/../.env' => base_path('.env'),
+        ], '');
+
         $this->publishes([__DIR__.'/../frontend' => base_path('frontend')], 'frontend');
     }
 
     public function register()
     {
+        // register the current package
+        $this->app->bind('base', function ($app) {
+            return new Base($app);
+        });
+
+        $this->app->register('Phobo\TestPublish\TestPublishServiceProvider');
+
         $this->mergeConfigFrom(__DIR__.'/../config/translatable.php', 'translatable');
 
 	$this->commands([
